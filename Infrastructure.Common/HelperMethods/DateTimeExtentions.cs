095bbb04d7690e1d75667b9d37d14f61f3ed5d95
Michael Visk <cooker2007>
2016-01-25 00:59:03 -0600

    Doc comments added to DateTime extension methods

--- Infrastructure.Common/HelperMethods/DateTimeExtentions.cs
+++ Infrastructure.Common/HelperMethods/DateTimeExtentions.cs
@@ -2,14 +2,42 @@
 {
     public static class DateTimeHelper
     {
-       public static DateTime? ParseNullableDateTime(string s)
-       {
-           DateTime parsedDateTime;
-           DateTime? nullableDateTime = DateTime.TryParse(s, out parsedDateTime) ? parsedDateTime : (DateTime?)null;
-           return nullableDateTime;
-       }
+        /// <summary>
+        /// Converts the string representation of a date and time to its <see cref="T:Nullable{System.DateTime}"/> equivalent.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static DateTime? ParseNullableDateTime(string input)
+        {
+            DateTime parsedDateTime;
+            var nullableDateTime = DateTime.TryParse(input, out parsedDateTime) ? parsedDateTime : (DateTime?) null;
+            return nullableDateTime;
+        }
 
-        public static bool IsBefore(this DateTime first,DateTime second)
+        /// <summary>
+        /// Compares the value of this instance to a specified <see cref="T:System.DateTime"/> value and returns a <see cref="T:bool"/> that indicates whether this instance is before than or after than the specified <see cref="T:System.DateTime"/> value.
+        /// </summary>
+        /// <param name="dateTimeA">This <see cref="T:System.DateTime"/> instance.</param>
+        /// <param name="dateTimeB">The <see cref="T:System.DateTime"/> to compare.</param>
+        /// <returns>
+        /// True if <paramref name="dateTimeA"/> is before <paramref name="dateTimeB"/>.
+        /// False if <paramref name="dateTimeA"/> is equal or after <paramref name="dateTimeB"/>.
+        /// </returns>
+        public static bool IsBefore(this DateTime dateTimeA, DateTime dateTimeB)
+        {
+            return dateTimeA.CompareTo(dateTimeB) < 0;
+        }
+
+        /// <summary>
+        /// Compares the value of this instance to a specified <see cref="T:Nullable{System.DateTime}"/> value and returns a <see cref="T:bool"/> that indicates whether this instance is before than or after than the specified <see cref="T:Nullable{System.DateTime}"/> value.
+        /// </summary>
+        /// <param name="dateTimeA">This <see cref="T:Nullable{System.DateTime}"/> instance.</param>
+        /// <param name="dateTimeB">The <see cref="T:Nullable{System.DateTime}"/> to compare.</param>
+        /// <returns>
+        /// True if the value of <paramref name="dateTimeA"/> is before the value of <paramref name="dateTimeB"/>,
+        /// false if the value of <paramref name="dateTimeA"/> is equal or after the value of <paramref name="dateTimeB"/>.
+        /// </returns>
+        public static bool IsBefore(this DateTime? dateTimeA, DateTime? dateTimeB)
         {
             if (dateTimeA.HasValue && dateTimeB.HasValue)
             {
